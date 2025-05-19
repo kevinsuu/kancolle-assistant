7d08f4e07ac9c90feaaae24976dcfcd6fedc90c7
planetarian <planetarian@users.noreply.github.com>
2025-05-19 16:17:28 -0500

    extension installation crypto api fix

--- packages/electron-chrome-web-store/src/browser/installer.ts
+++ packages/electron-chrome-web-store/src/browser/installer.ts
@@ -1,6 +1,7 @@
 import * as fs from 'node:fs'
 import * as os from 'node:os'
 import * as path from 'node:path'
+import * as crypto from 'node:crypto'
 import { Readable } from 'node:stream'
 import { pipeline } from 'node:stream/promises'
 import { session as electronSession } from 'electron'
